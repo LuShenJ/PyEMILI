b0447a5ec0e52bb294775bdab974f0d157899258
LuShenJ <745417397@qq.com>
2024-11-06 15:22:23 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-version = '1.0.0'
+version = '1.0.1'
 
 from setuptools import setup, find_packages
 import os
@@ -11,12 +11,11 @@ with open('requirements.txt') as f:
 setup(name='pyemili', 
       version=version,
       description='Spectral lines identifier',
-      long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+      long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst'),encoding='utf-8').read(),
       author='Zhijun Tu, Xuan Fang, Robert Williams, Jifeng Liu',
       author_email='zjtu@bao.ac.cn',
       url='https://github.com/LuShenJ/PyEMILI',
       install_requires=requirements,
-      include_package_data=True,
       packages=find_packages(include=['pyemili']),
       package_data={'pyemili':['abundance/*.dat',
                         'Line_dataset/*.dat',
