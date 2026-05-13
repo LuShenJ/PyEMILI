e87cc1f26d7947d0fee5ec5a7efbeeb3c1ac79c4
庐参 <56467275+LuShenJ@users.noreply.github.com>
2026-05-13 19:25:51 +0800

    Bump package version to 1.1.0

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-version = '1.1'
-time = '2025.11'
+version = '1.1.0'
+time = '2026.05'
 from setuptools import setup, find_packages
 import os
 
@@ -8,21 +8,22 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 
-setup(name='pyemili', 
+setup(name='pyemili',
       version=version,
       description='Spectral lines identifier',
-      long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst'),encoding='utf-8').read(),
+      long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8').read(),
+      long_description_content_type='text/x-rst',
       author='Zhijun Tu, Xuan Fang, Robert Williams, Jifeng Liu',
       author_email='zjtu@bao.ac.cn',
       url='https://github.com/LuShenJ/PyEMILI',
       install_requires=requirements,
       packages=find_packages(include=['pyemili']),
-      package_data={'pyemili':['abundance/*.dat',
-                        'Line_dataset/*.dat',
-                        'Line_dataset/*.npy',
-                        'Line_dataset/*.npz',
-                        'recom/*.dat',
-                        'eff_reccoe/*.dat',
-                        'eff_reccoe/*.npy',]
+      package_data={'pyemili': ['abundance/*.dat',
+                                'Line_dataset/*.dat',
+                                'Line_dataset/*.npy',
+                                'Line_dataset/*.npz',
+                                'recom/*.dat',
+                                'eff_reccoe/*.dat',
+                                'eff_reccoe/*.npy',]
                     },
 )
